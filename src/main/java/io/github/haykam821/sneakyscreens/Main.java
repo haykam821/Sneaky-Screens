3460803e11d8fad9bac082478ef109128e1f7930
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-01-08 19:22:34 -0500

    Prevent sneaking in a screen while flying

--- src/main/java/io/github/haykam821/sneakyscreens/Main.java
+++ src/main/java/io/github/haykam821/sneakyscreens/Main.java
@@ -5,6 +5,8 @@ import net.minecraft.client.MinecraftClient;
 public class Main {
 	public static boolean toggleGetter(boolean sneakToggled, MinecraftClient client) {
 		boolean screenVisible = client.currentScreen != null;
-		return sneakToggled || screenVisible;
+		boolean sneakInContainer = screenVisible && client.player != null && client.player.canFly();
+
+		return sneakToggled || sneakInContainer;
 	}
 }
